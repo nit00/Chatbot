85fad569d48f990329ba294afbebce919e481957
nit00 <nitin19.myid@gmail.com>
2024-01-18 20:32:49 +0530

    Added Gemini model

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 from langchain.chains.llm import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_community.llms import HuggingFaceHub
+from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 import requests
 
@@ -16,7 +17,7 @@ text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 docs = text_splitter.split_documents(documents)
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 embeddings =GPT4AllEmbeddings()
-
+os.environ["GOOGLE_API_KEY"]="AIzaSyC1v_n6qGnxEbQ1RLLkDjooFaWximsp0B4"
 db = FAISS.from_documents(docs,embeddings)
 
 query = "Where was Robinson born?"
@@ -24,16 +25,17 @@ docs = db.similarity_search(query)
 text=(docs[0].page_content)
 #LangchainSummarization
 
-repo_id="google/flan-t5-xxl"
-llm = HuggingFaceHub(
-    repo_id=repo_id, model_kwargs={"temperature": 0.5}
-)
-
+# repo_id="google/flan-t5-xxl"
+# llm = HuggingFaceHub(
+#     repo_id=repo_id, model_kwargs={"temperature": 0.5}
+# )
+llm = ChatGoogleGenerativeAI(model="gemini-pro")
 template = """Answer the given question by using the below given context:
 Question: {query},
 Context: {text}
 
-Answer to the query:"""
+And give response in a summarised manner in 50 words.
+"""
 
 prompt = PromptTemplate(template=template, input_variables=["query","text"])
 llm_chain = LLMChain(prompt=prompt, llm=llm)
